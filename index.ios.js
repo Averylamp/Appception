8a3ad4816f3203f23f0c0ef391fcc684ff28ae04
Avery Lamp <averylamp@gmail.com>
2016-02-13 03:17:57 -0800

    initial stuff

--- index.ios.js
+++ index.ios.js
@@ -7,46 +7,70 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  Image,
   Text,
-  View
+  View,
+  ListView,
+  TouchableHighlight
 } from 'react-native';
 
-class Appception extends Component {
+var objects = ['LABEL','BUTTON','MAP', 'LIST','PIN'];
+
+var Appception = React.createClass({
+
+
+    getInitialState: function() {
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      return {
+        dataSource: ds.cloneWithRows(objects),
+      };
+  },
+
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+           <ListView style={styles.listViewStyle}
+      dataSource={this.state.dataSource}
+      renderRow={(rowData) => <TouchableHighlight  style={styles.buttonStyle}><Text style={styles.buttonTextStyle}>{rowData}</Text></TouchableHighlight>}
+    />
     );
   }
-}
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  listViewStyle: {
+    borderTopWidth: 50
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonStyle: {
+
+    height:70,
+    backgroundColor:'#2196f3',
+    borderWidth: 10,
+    borderTopWidth:5,
+    borderBottomWidth:5,
+    borderRadius: 6,
+    shadowOffset:{
+      height:1,
+      width:1
+    },
+    borderRadius:15,
+    borderColor: 'white',
+    alignItems: 'center',
   },
+  buttonTextStyle: {
+    fontSize: 18,
+    backgroundColor:'transparent',
+    marginTop:17,
+    fontFamily:'Geeza Pro',
+    color:'white'
+  }
 });
 
 AppRegistry.registerComponent('Appception', () => Appception);
