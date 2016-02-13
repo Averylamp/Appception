756e9bacda3f98a181047e64069a5028adc0334a
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2016-02-13 13:36:09 -0800

    fixed arbitrary width stuff

--- Droppable.js
+++ Droppable.js
@@ -15,7 +15,9 @@ export default class Droppable extends Component {
   isDropZone(gesture) {
       var dz = this.state.dropZoneValues;
       // TODO: only works heightwise (for now)
-      return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
+      let inHeight = (gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height);
+      let inWidth = (gesture.moveX > dz.x && gesture.moveX < dz.x + dz.width);
+      return inWidth && inHeight
   }
   render() {
     return (
